a349032e6b30a4ed94d086b1f45889cbd7fabb25
Pedro Sanders <psanders@fonoster.com>
2021-09-02 14:27:42 -0400

    Update index.js

--- src/service/index.js
+++ src/service/index.js
@@ -1,1 +1,21 @@
+import express from "express";
+const app = express();
 
+app.use(express.json());
+const port = 3000;
+
+app.get('/instrumentation', async(req, res) => {
+  const instrumentationRequest = {
+    instrumentationServer: "",
+    projectId: "project001"
+  }
+
+  // Validate body
+  // Validate request origin
+  // Generate instrumentation
+  res.send(200);
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}/instrumentation`)
+})
